c66255296ba00b9b46aab86748407875d1e04c64
npc <nick_paw@hotmail.com>
2017-09-28 14:53:06 +0200

    Chart fixes

--- src/app/widgets/htm-chart/htm-chart.component.ts
+++ src/app/widgets/htm-chart/htm-chart.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { WidgetComponent } from '../../services/widgetLibrary-service/widget.component';
-import { GooglechartsService } from '../../services/googlechart/googlechart.service';
+import { GooglechartsService } from '../../services/googlecharts/googlecharts.service';
 import { Http, Response } from '@angular/http';
 
 import * as $ from 'jquery';
@@ -9,8 +9,7 @@ declare var swal: any;
 @Component({
   selector: '[app-htm-chart]',
   templateUrl: './htm-chart.component.html',
-  styleUrls: ['./htm-chart.component.css'],
-  providers: [GooglechartsService]
+  styleUrls: ['./htm-chart.component.css']
 })
 
 export class HtmChartComponent implements OnInit, WidgetComponent {
