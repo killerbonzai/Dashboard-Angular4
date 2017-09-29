818e04b80a9c60bc81a4893fc1ca690514d500b6
npc <nick_paw@hotmail.com>
2017-09-29 15:32:43 +0200

    Hardcoded line chart PoC + start config set to htm map and chart

--- src/app/services/user-service/user.service.ts
+++ src/app/services/user-service/user.service.ts
@@ -20,8 +20,8 @@ export class UserService {
 
     //Test - Last parameter is null, so a configuration will be generated
     if(this.user == undefined){
-      let d = new Dashboard(1, "Standard1Col", 1, []);
-      let d2 = new Dashboard(2, "Content2col", 4, []);
+      let d = new Dashboard(1, "Standard1Col", 1, [10,12]);
+      let d2 = new Dashboard(2, "Content2col", 4, [10,12]);
       let das = [];
       das.push(d);
       das.push(d2);
